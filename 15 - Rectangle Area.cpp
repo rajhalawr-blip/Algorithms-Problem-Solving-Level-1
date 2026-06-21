1ea0d497cb9114153cdfff9be6163bfc231b6449
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-06-21 15:19:34 +0300

    Rectangle Area

---
+++ 15 - Rectangle Area.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void ReadNumbers(float& Length, float& Width)
+{
+    cout << "Please Enter Length?\n";
+    cin >> Length;
+
+    cout << "Please Enter Width?\n";
+    cin >> Width;
+}
+
+float CalculateRectangleArea(float Length, float Width)
+{
+    return Length * Width;
+}
+
+void PrintResult(float Area)
+{
+    cout << "\nRectangle Area = " << Area << endl;
+}
+
+int main()
+{
+    float Length, Width;
+
+    ReadNumbers(Length, Width);
+    PrintResult(CalculateRectangleArea(Length, Width));
+
+    return 0;
+}
