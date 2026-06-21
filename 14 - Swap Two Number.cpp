a958d724fa91923d839b0a220988796f7719415b
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-06-21 15:18:59 +0300

    Swap Two Number

---
+++ 14 - Swap Two Number.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void ReadNumbers(int& num1, int& num2)
+{
+    cout << "Please Enter Number 1?\n";
+    cin >> num1;
+
+    cout << "Please Enter Number 2?\n";
+    cin >> num2;
+}
+
+void Swap(int& num1, int& num2)
+{
+    int Temp;
+
+    Temp = num1;
+    num1 = num2;
+    num2 = Temp;
+}
+
+void PrintNumbers(int num1, int num2)
+{
+    cout << "\nNumber1 = " << num1 << endl;
+    cout << "Number2 = " << num2 << endl;
+}
+
+int main()
+{
+    int num1, num2;
+
+    ReadNumbers(num1, num2);
+    PrintNumbers(num1, num2);
+    Swap(num1, num2);
+    PrintNumbers(num1, num2);
+
+    return 0;
+}
