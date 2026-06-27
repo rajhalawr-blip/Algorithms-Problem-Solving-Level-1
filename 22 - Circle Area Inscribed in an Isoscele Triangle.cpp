0b2583e3c69c507b8a7a29a118a1d0d67eb0a01a
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-06-27 15:56:18 +0300

    Add files via upload

---
+++ 22 - Circle Area Inscribed in an Isoscele Triangle.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+void ReadNumbers(float& A, float& B)
+{
+    cout << "Please Enter A?\n";
+    cin >> A;
+
+    cout << "Please Enter B?\n";
+    cin >> B;
+}
+
+float CircleAreaByIsosclelesTriangle(float A, float B)
+{
+    const float PI = 3.141592654;
+
+    return (PI * pow(B, 2)) / 4 * ((2 * A - B) / (2 * A + B));
+}
+
+void PrintResult(float Area)
+{
+    cout << "\nCircle Area = " << Area << endl;
+}
+
+int main()
+{
+    float A, B;
+
+    ReadNumbers(A, B);
+    PrintResult(CircleAreaByIsosclelesTriangle(A, B));
+
+    return 0;
+}
