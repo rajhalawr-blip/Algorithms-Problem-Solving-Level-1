b2ce78b284d094e5005b13815097a1ed3cb89180
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-06-27 15:54:56 +0300

    Add files via upload

---
+++ 18 - Circle Area.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+float ReadNumbers()
+{
+    float r;
+
+    cout << "Please Enter R?\n";
+    cin >> r;
+
+    return r;
+}
+
+float CalculateCircleArea(float R)
+{
+    const float PI = 3.141592654;
+
+    return PI * pow(R, 2);
+}
+
+void PrintResult(float Area)
+{
+    cout << "\nCircle Area = " << Area << endl;
+}
+
+int main()
+{
+
+    PrintResult(CalculateCircleArea(ReadNumbers()));
+
+    return 0;
+}
