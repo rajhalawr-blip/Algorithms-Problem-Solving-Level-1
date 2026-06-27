b96eb13d957297b2bad878452e4cd5e9eda163ca
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-06-27 15:55:56 +0300

    Add files via upload

---
+++ 21 - Circle Area Along The Circumference.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+float ReadCireumference()
+{
+    float L;
+
+    cout << "Please Enter The Cireumference L?\n";
+    cin >> L;
+
+    return L;
+}
+
+float CircleAreaByCireumference(float L)
+{
+    const float PI = 3.141592654;
+
+    return pow(L, 2) / (4 * PI);
+}
+
+void PrintResult(float Area)
+{
+    cout << "\nCircle Area = " << Area << endl;
+}
+
+int main()
+{
+
+    PrintResult(CircleAreaByCireumference(ReadCireumference()));
+
+    return 0;
+}
