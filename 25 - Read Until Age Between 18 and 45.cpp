c2acec5c380ae38a87d6776b810a43fc09a8dd36
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-06-27 15:57:09 +0300

    Add files via upload

---
+++ 25 - Read Until Age Between 18 and 45.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadAge()
+{
+    int Age;
+
+    cout << "Please Enter Your Age Between From 18 and 45?\n";
+    cin >> Age;
+
+    return Age;
+}
+
+bool ValidateNumberInRange(int Number, int From, int To)
+{
+    return Number >= From && Number <= To;
+}
+
+int ReadUntilAgeBetween(int From, int To)
+{
+    int Age = 0;
+
+    do
+    {
+        Age = ReadAge();
+
+    } while (!ValidateNumberInRange(Age, From, To));
+
+    return Age;
+}
+
+void PrintResulte(int Age)
+{
+    cout << "\nYour Age is: " << Age << endl;
+}
+
+int main()
+{
+    PrintResulte(ReadUntilAgeBetween(18, 45));
+
+    return 0;
+}
