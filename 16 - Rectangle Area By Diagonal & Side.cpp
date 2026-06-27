84ed3949551a5eca878ac5cf74245b3f6fbb53c7
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-06-27 15:53:44 +0300

    Add files via upload

---
+++ 16 - Rectangle Area By Diagonal & Side.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+void ReadNumbers(float& A, float& D)
+{
+    cout << "Please Enter A?\n";
+    cin >> A;
+
+    cout << "Please Enter D?\n";
+    cin >> D;
+}
+
+float RectangleAreaBySideAndDiagonal(float A, float D)
+{
+    return A * sqrt(pow(D, 2) - pow(A, 2));
+}
+
+void PrintResult(float Area)
+{
+    cout << "\nRectangle Area = " << Area << endl;
+}
+
+int main()
+{
+    float A, D;
+
+    ReadNumbers(A, D);
+    PrintResult(RectangleAreaBySideAndDiagonal(A, D));
+
+    return 0;
+}
