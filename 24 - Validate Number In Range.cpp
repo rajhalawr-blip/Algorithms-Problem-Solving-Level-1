e7f3b3181509d1ee2335278d274d8c4eb0607172
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-06-27 15:56:54 +0300

    Add files via upload

---
+++ 24 - Validate Number In Range.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadAge()
+{
+    int Age;
+
+    cout << "Please Enter Your Age?\n";
+    cin >> Age;
+
+    return Age;
+}
+
+bool ValidateNumberInRange(int Number, int From, int To)
+{
+    return Number >= From && Number <= To;
+}
+
+void PrintResulte(int Age)
+{
+    if (ValidateNumberInRange(Age, 18, 45))
+        cout << Age << " is a valid age \n";
+    else
+        cout << Age << " is invalid age \n";
+}
+
+int main()
+{
+    PrintResulte(ReadAge());
+
+    return 0;
+}
