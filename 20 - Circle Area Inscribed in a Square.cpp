0da7acf53c202b5b2843bdbebddbe933ecc5f041
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-06-27 15:55:39 +0300

    Add files via upload

---
+++ 20 - Circle Area Inscribed in a Square.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+float ReadNumbers()
+{
+    float A;
+
+    cout << "Please Enter A?\n";
+    cin >> A;
+
+    return A;
+}
+
+float CircleAreaInscribedInSquare(float A)
+{
+    const float PI = 3.141592654;
+
+    return (PI * pow(A, 2)) / 4;
+}
+
+void PrintResult(float Area)
+{
+    cout << "\nCircle Area = " << Area << endl;
+}
+
+int main()
+{
+
+    PrintResult(CircleAreaInscribedInSquare(ReadNumbers()));
+
+    return 0;
+}
