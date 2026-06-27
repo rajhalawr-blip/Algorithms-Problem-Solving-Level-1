3245cb08591c1c32b95048a7f88ec14188b4d129
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-06-27 15:54:27 +0300

    Add files via upload

---
+++ 17 - Triangle Area.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+void ReadNumbers(float& A, float& H)
+{
+    cout << "Please Enter A?\n";
+    cin >> A;
+
+    cout << "Please Enter H?\n";
+    cin >> H;
+}
+
+float CalculateTriangleArea(float A, float H)
+{
+    return A / 2 * H;
+}
+
+void PrintResult(float Area)
+{
+    cout << "\nTriangle Area = " << Area << endl;
+}
+
+int main()
+{
+    float A, H;
+
+    ReadNumbers(A, H);
+    PrintResult(CalculateTriangleArea(A, H));
+
+    return 0;
+}
