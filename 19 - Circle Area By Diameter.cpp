2cd75d8c99ca768bc19bc63ebc137ef25e4ac4e7
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-06-27 15:55:13 +0300

    Add files via upload

---
+++ 19 - Circle Area By Diameter.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+float ReadNumbers()
+{
+    float D;
+
+    cout << "Please Enter D?\n";
+    cin >> D;
+
+    return D;
+}
+
+float CalculateCircleAreaByDiameter(float D)
+{
+    const float PI = 3.141592654;
+
+    return (PI * pow(D, 2)) / 4;
+}
+
+void PrintResult(float Area)
+{
+    cout << "\nCircle Area = " << Area << endl;
+}
+
+int main()
+{
+
+    PrintResult(CalculateCircleAreaByDiameter(ReadNumbers()));
+
+    return 0;
+}
