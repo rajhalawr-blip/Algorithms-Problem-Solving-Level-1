47974fc5e1baa85f61f4a313608fd37f2207bbdc
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-06-28 17:58:02 +0300

    Print Numbers From N to 1

---
+++ 27 - Print Numbers From N to 1.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int ReadNumber()
+{
+    int N;
+
+    cout << "Please enter a number?\n";
+    cin >> N;
+
+    return N;
+}
+
+void PrintRangeFormNto1_UsingWhile(int N)
+{
+    int Counter = ++N;
+
+    cout << "Range printed using While Statement:\n";
+
+    while (Counter > 1)
+    {
+        Counter--;
+        cout << Counter << endl;
+    }
+}
+
+void PrintRangeFormNto1_UsingDoWhile(int N)
+{
+    int Counter = ++N;
+
+    cout << "Range printed using Do..While Statement:" << endl;
+
+    do
+    {
+        Counter--;
+        cout << Counter << endl;
+
+    } while (Counter > 1);
+}
+
+void PrintRangeFormNto1_UsingFor(int N)
+{
+
+    cout << "Range printed using for Statement:" << endl;
+
+    for (int Counter = N; Counter >= 1; Counter--)
+    {
+        cout << Counter << endl;
+    }
+}
+
+
+int main()
+{
+    int N = ReadNumber();
+
+    PrintRangeFormNto1_UsingWhile(N);
+    PrintRangeFormNto1_UsingDoWhile(N);
+    PrintRangeFormNto1_UsingFor(N);
+
+    return 0;
+}
