29d61692e1f218781571e028581f737ae7693b8e
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-07-04 14:18:44 +0300

    Power of 2.3.4

---
+++ 31 - Power of 2.3.4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadNumber()
+{
+    int N;
+    cout << "Please Enter a Number\n";
+    cin >> N;
+
+    return N;
+}
+
+void PowerOfE2E3E4(int Number)
+{
+    int a, b, c;
+
+    a = Number * Number;
+    b = Number * Number * Number;
+    c = Number * Number * Number * Number;
+
+    cout << a << " " << b << " " << c << endl;
+}
+
+int main()
+{
+    PowerOfE2E3E4(ReadNumber());
+
+    return 0;
+}
