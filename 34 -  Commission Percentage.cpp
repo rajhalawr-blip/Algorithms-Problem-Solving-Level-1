7305f701b80555a8722c4c81d095080897db83e4
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-07-04 14:20:23 +0300

    Commission Percentage

---
+++ 34 -  Commission Percentage.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadTotalSalse()
+{
+    int TotalSalse;
+    cout << "Please Enter Total Salse?\n";
+    cin >> TotalSalse;
+
+    return TotalSalse;
+}
+
+float GetComissionPercentage(int TotalSalse)
+{
+    if (TotalSalse > 1000000)
+        return 0.01;
+    else if (TotalSalse > 500000)
+        return 0.02;
+    else if (TotalSalse > 10000)
+        return 0.03;
+    else if (TotalSalse >= 50000)
+        return 0.05;
+    else
+        return 0.00;
+}
+
+float CalculateTotalComission(float TotalSalse)
+{
+    return GetComissionPercentage(TotalSalse) * TotalSalse;
+}
+
+int main()
+{
+    int TotalSalse = ReadTotalSalse();
+
+    float ComissionPercentage = GetComissionPercentage(TotalSalse);
+    float TotalComission = CalculateTotalComission(TotalSalse);
+
+    cout << endl << "Comission Percentage = " << ComissionPercentage << endl;
+    cout << endl << "Total Comission = " << TotalComission << endl;
+
+    return 0;
+}
