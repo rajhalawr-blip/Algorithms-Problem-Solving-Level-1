977827217b9f5fccb482a8cbdda29b88b67520b3
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-07-04 14:19:20 +0300

    Power of M

---
+++ 32 - Power of M.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadNumber()
+{
+    int Number;
+    cout << "Please Enter a Number?\n";
+    cin >> Number;
+
+    return Number;
+}
+
+int ReadPower()
+{
+    int Number;
+    cout << "Please Enter The Power?\n";
+    cin >> Number;
+
+    return Number;
+}
+
+int PowerNumberOfM(int M, int Number)
+{
+    if (M == 0)
+    {
+        return 1;
+    }
+
+    int P = 1;
+
+    for (int i = 1; i <= M; i++)
+    {
+        P *= Number;
+    }
+
+    return P;
+}
+
+int main()
+{
+    cout << endl << PowerNumberOfM(ReadPower(), ReadNumber()) << endl;
+
+    return 0;
+}
