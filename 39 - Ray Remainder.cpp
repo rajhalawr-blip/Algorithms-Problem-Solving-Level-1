4c1ec7a6a490e0dedf6c6fdd1d3ce12418a02125
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-07-06 14:19:32 +0300

    Ray Remainder

---
+++ 39 - Ray Remainder.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+float ReadPositiveNumber(string Message)
+{
+	int Number;
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+	} while (Number < 0);
+	
+	return Number;
+}
+
+float CalculateRemainder(float TotalBill, float TotalCachPaid)
+{
+	return TotalCachPaid - TotalBill;
+}
+
+int main()
+{
+	float TotalBill = ReadPositiveNumber("Please enter Total Bill?");
+	float TotalCashPaid = ReadPositiveNumber("Please enter Cash Paid?");
+
+	cout << endl;
+	cout << "Total Bill = " << TotalBill << endl;
+	cout << "Total Cah Paid = " << TotalCashPaid << endl;
+
+	cout << "************************" << endl;
+	cout << "Remainder = " << CalculateRemainder(TotalBill, TotalCashPaid) << endl;
+
+	return 0;
+}
