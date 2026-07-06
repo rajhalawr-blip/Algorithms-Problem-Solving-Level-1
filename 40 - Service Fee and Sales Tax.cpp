d91a68de480f8f39599bf4d85b702aecdfa68a88
Rajh Abu-Alawr <rajhalawr@gmail.com>
2026-07-06 14:19:54 +0300

    Service Fee and Sales Tax

---
+++ 40 - Service Fee and Sales Tax.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+float ReadPositiveNumber(string Message)
+{
+	float Number;
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+
+	return Number;
+}
+
+float TotalBillAfterServiceAndTax(float TotalBill)
+{
+	TotalBill = TotalBill * 1.1;
+	TotalBill = TotalBill * 1.16;
+
+	return TotalBill;
+}
+
+int main()
+{
+	float TotalBill = ReadPositiveNumber("Please enter Total Bill?");
+
+	cout << endl;
+	cout << "Total Bill = " << TotalBill << endl;
+	cout << "Total Bill After Service Fee and Sales Tax = "
+		<< TotalBillAfterServiceAndTax(TotalBill) << endl;
+
+	return 0;
+}
